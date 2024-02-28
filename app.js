423ae54ca061403e9b759defc1f1e990d75608e5
Silas Leite <bigchill_silas@hotmail.com>
2024-02-28 10:51:14 -0300

    feat: Changes in register user, now it is using querystring and route that detailed products, now it detailing a car using information send in params

--- app.js
+++ app.js
@@ -2,27 +2,29 @@ const express = require('express');
 const app = express();
 
 
-app.listen(3003, () => {
+app.listen(8081, () => {
     console.log('Server is running');
 });
 
-app.get('/cadastro', (request, response) => {
-    response.send('Página de cadastro!');
+app.get('/', (request, response) => {
+    response.send('Página principal')
+});
+
+app.get('/cadastro/usuario', (request, response) => {
+    const user = {
+        name: request.query.name,
+        lastname: request.query.lastname,
+        email: request.query.email,
+    };
+    const responseBody = `Página de cadastro!\nUsuario ${user.name} ${user.lastname} (${user.email}) cadastro com sucesso!`
+    
+    response.send(responseBody);
 });
 
 app.get('/contato', (request, response) => {
     response.send('Página de contato!');
 });
 
-app.get('/produto/:item', (request, response) => {
-    response.send(`Página de detalhes do produto ${request.params.item}`);
-});
-
-app.get('/produto/:category/:color', (request, response) => {
-    const product = `Página de produtos da categoria ${request.params.category} e cor: ${request.params.color}`;
-    response.send(product);
-});
-
-app.get('/', (request, response) => {
-    response.send('Página principal')
+app.get('/produto/:automovel/:marca/:modelo/:ano', (request, response) => {
+    response.send(`Página de detalhes do produto ${request.params.automovel}, modelo ${request.params.marca} - Marca: ${request.params.marca} do ano de ${request.params.ano}`);
 });
